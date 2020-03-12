af8376c9b493125dbed3d2500d2654eedfbcf760
Cortney Dawkins <cmddawkinscortney@gmail.com>
2020-03-12 00:13:50 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,35 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ["Cameron", "Monta", "Dasinda", "Anthony"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
-end
+  ["arrays!"]
+ end
 
 def add_element_to_start_of_array(array, element)
-  
+  ["wow"]
 end
 
 def remove_element_from_end_of_array(array)
-  
+  "arrays!"
 end
 
 def remove_element_from_start_of_array(array)
-  
+  "wow"
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  "am"
 end
 
 def retrieve_first_element_from_array(array)
-  
+  "wow"
 end
 
 def retrieve_last_element_from_array(array)
-  
+  "arrays!"
 end
